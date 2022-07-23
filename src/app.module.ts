d82a12678f4707c43317a2ed0d5563ed8e9234de
JackDeng666 <1556393081@qq.com>
2022-07-23 20:39:21 +0800

    config引入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static serverPort: number;
+  static serverVersion: string;
+  static serverPrefix: string;
+
+  constructor(private readonly configService: ConfigService) {
+    AppModule.serverPort = +this.configService.get('SERVER_PORT');
+    AppModule.serverVersion = this.configService.get('SERVER_VERSION');
+    AppModule.serverPrefix = this.configService.get('SERVER_PREFIX');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix(AppModule.serverPrefix);
+  await app.listen(AppModule.serverPort);
+  return {
+    serverPort: AppModule.serverPort,
+    serverPrefix: AppModule.serverPrefix,
+  };
 }
-bootstrap();
+bootstrap().then(({ serverPort, serverPrefix }) => {
+  Logger.log(
+    `Application running on: http://localhost:${serverPort}${serverPrefix}`,
+    'Main',
+  );
+});
